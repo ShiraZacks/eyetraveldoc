459ac3c42213b03516896a9e1ec8ebadc475a548
Shira Zacks <shiramzacks@gmail.com>
2022-09-18 16:09:04 -0400

    added logo to page

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eye Travel: Eye Care For The Homebound</title>
+    <title>Eye Travel: Eye Care For The Home Bound</title>
+    <link rel="shortcut icon" type="image/png" href="logo.png" />
     <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
     <link rel="stylesheet" href="style.css?<?= time() ?>"></head>
+    
 <?php include "menu.php" ;?>
 <body>
-    
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -2,15 +2,22 @@
 echo ' <!-- Navbar  -->
     <div class="top">
         <div class="topBar">
+
             <div class="smaller">
-                <a class="bar" onclick="myFunction()" title="Toggle Navigation Menu"><img
-                        src="bars.svg" alt="MENU"></a>
+                <a class="bar" onclick="myFunction()" title="Toggle Navigation Menu"><img src="bars.svg" alt="MENU"></a>
             </div>
+
+
+
             <a href="index.php" class="buttons">Home</a>
             <a href="parsha.php" class="buttons">This Week\'s Parsha</a>
             <a href="month.php" class="buttons">This Month</a>
             <a href="yontif.php" class="buttons">Next Yom Tov</a>
             <a href="contact.php" class="buttons">Contact Me</a>
+        </div>
+    <!-- eye travel logo-->
+        <div>
+            <img class="logo" src="logo.png" alt="Eye Travel: Eye Care for the Home Bound">
         </div>
 
         <!-- Navbar on small screens -->
@@ -23,4 +30,4 @@ echo ' <!-- Navbar  -->
                 <li><a href="contact.php" class="button bottom">Contact Me</a></li>
             </ul>
         </div>
-    </div>';?>
+    </div>';
