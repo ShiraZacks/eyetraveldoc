3d09744cd2a4962bbb1b53a83ffda05a8c828b42
Shira Zacks <shiramzacks@gmail.com>
2022-09-18 12:47:58 -0400

    add style and composer

--- menu.php
+++ menu.php
@@ -1,3 +1,12 @@
-<div>
-    
-</div>
+<?php
+echo '<div class="topBar">
+        <a class="coolButton"  href="home.php">HOME</a>
+
+        <a class="coolButton" href="editorsChoice.php">Editor&#x2019s Choice Recipes</a>
+
+        <a class="coolButton" href="calendar.html">Jewish Calendar Page of Helpfulness</a>
+
+        <a class="coolButton" href="userRecipies.php">User Recipes</a>
+
+        </div> 
+    <div class="top"></div>';?>
