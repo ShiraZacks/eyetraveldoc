0b552d8f96357d527f47215fb92e2b00c4040629
Shira Zacks <shiramzacks@gmail.com>
2022-09-28 17:02:36 -0400

    working on css

--- index.php
+++ index.php
@@ -17,19 +17,36 @@
     <div class="wrap">
         <div class="parallax">
         </div>
-
         <div class="picText">
             <h1>EYE TRAVEL - MICHIGAN'S ONLY HOME CARE EYE DOCTORS</h1>
             <h1>CALL (248) 376-6046 TO SCHEDULE AN APPOINTMENT</h1>
-
         </div>
     </div>
+
+    <hr class="hr-style">
 
     <div class="middle">
         <h1 class="center">OUR SERVICE</h1>
         <h3 class="center">Eye Travel has focused on providing residents of Southeastern Michigan with top-quality eye care. Using state of the art portable medical equipment, each patient receives a comprehensive eye exam. The exam begins with the doctor testing the patient's vision. The doctor continues to give a full eye exam, checking the overall health of the eye.</h3>
         <h3 class="center">If you have any questions you can call our office at (248)376-6046 or send an email to drzacks@eyetraveldoc.com</h3>
     </div>
+
+    <div class="row">
+        <div class="column">
+            <ul><b>EYE TESTS</b></ul>
+            <hr>
+        </div>
+        <div class="column">
+            <ul><b>OPTICAL SHOP</b></ul>
+            <hr>
+        </div>
+        <div class="column">
+            <ul><b>SCHEDULE APPOINTMENT</b></ul>
+            <hr>
+            <ul></ul>
+        </div>
+    </div>
+
 </body>
 
 </html>
