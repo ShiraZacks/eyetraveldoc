03f894d4cd5d55ca31922cbb5161423658d95bf9
Shira Zacks <shiramzacks@gmail.com>
2022-10-21 12:13:17 -0400

    ADDED NEXT SEGMENT

--- index.php
+++ index.php
@@ -34,10 +34,10 @@
 
     <div class="row">
         <div class="column">
-        <div class="center">
-            <img class="circle-pic" src="eye.png">
-        </div>
-            <ul><b>EYE TESTS</b></ul> 
+            <div class="center">
+                <img class="circle-pic" src="eye.png">
+            </div>
+            <ul><b>EYE TESTS</b></ul>
             <hr>
             <li>Glaucoma test</li>
             <li>Cataract evaluation</li>
@@ -46,9 +46,9 @@
             <li>Fundus photography</li>
         </div>
         <div class="column">
-        <div class="center">
-            <img class="circle-pic" src="glasses.jpg">
-        </div>
+            <div class="center">
+                <img class="circle-pic" src="glasses.jpg">
+            </div>
             <ul><b>OPTICAL SHOP</b></ul>
             <hr>
             <li>We bring a nice selection of frames</li>
@@ -56,20 +56,45 @@
             <li>Delivered to the house within 2 weeks of payment</li>
         </div>
         <div class="column">
-        <div class="center">
-            <img class="circle-pic" src="appt.jpg">
-        </div>
+            <div class="center">
+                <img class="circle-pic" src="appt.jpg">
+            </div>
             <ul><b>SCHEDULE APPOINTMENT</b></ul>
             <hr>
             <p class="appt-text">If you, or someone you care for, need to schedule an appointment with Eye Travel, call (248) 376-6046 and we will be in touch with you as soon as possible</p>
         </div>
     </div>
-
+<br>
     <div class="big-blue-stripe">
-        <br><br>
+        <br>
     </div>
 
-
+<div>
+    <h2>WHAT IS EYE TRAVEL</h2>
+    <p>Eye Travel is Michigans first and only eye care practice that specializes in home care. 
+        Our doctors are able to provide our patients with the same care that they would receive in a traditional eye doctors office. 
+        All of the equipment they need fits nicely in a suitcase, and is so portable that the exam can given whether 
+        the patient prefers to sit in their favorite chair or even if they are bed bound. 
+        Whether you're a home care professional, a caregiver, or just a friend of someone 
+        who has difficulty getting to the doctors, contact Eye Travel today and help improve the 
+        quality of life of someone you care about. 
+        Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
+    
+    <div class="row">
+        <div class="column-2 shmush-right">
+            <h1>"Eye Travel is the first and only eye care practice in Michigan to focus 100% on home care"</h1>
+        </div>
+        <div class="column-2 shmush-left">
+            <p>Eye Travel is Michigan's first and only eye care practice that specializes in home care. 
+                Our doctors are able to provide our patients with the same care that they would receive 
+                in a traditional eye doctors office. All of the equipment they need fits nicely in a suitcase, 
+                and is so portable that the exam can be given whether the patient prefers to sit in their favorite 
+                chair or even if they are bed bound.</p>
+        </div>
+    </div>
+    
+    
+    </div>
 </body>
 
 </html>
