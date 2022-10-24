d84ebabcac6ab6e17cc9483e900dac3d962a0b0b
Shira Zacks <shiramzacks@gmail.com>
2022-10-24 11:53:51 -0400

    added most of the rest of the content, css changes

--- index.php
+++ index.php
@@ -22,15 +22,14 @@
             <h1>CALL (248) 376-6046 TO SCHEDULE AN APPOINTMENT</h1>
         </div>
     </div>
-    <hr class="style">
 
 
-    <div class="middle">
+    <div class="middle style">
         <h1 class="center">OUR SERVICE</h1>
         <h3 class="center">Eye Travel has focused on providing residents of Southeastern Michigan with top-quality eye care. Using state of the art portable medical equipment, each patient receives a comprehensive eye exam. The exam begins with the doctor testing the patient's vision. The doctor continues to give a full eye exam, checking the overall health of the eye.</h3>
         <h3 class="center">If you have any questions you can call our office at (248)376-6046 or send an email to drzacks@eyetraveldoc.com</h3>
     </div>
-    <hr class="style">
+    <br><br><br>
 
     <div class="row">
         <div class="column">
@@ -64,37 +63,103 @@
             <p class="appt-text">If you, or someone you care for, need to schedule an appointment with Eye Travel, call (248) 376-6046 and we will be in touch with you as soon as possible</p>
         </div>
     </div>
-<br>
+    <br>
+    <div class="big-blue-stripe">
+        <br>
+    <div class="white-text">
+        <h1 class="center">WHAT IS EYE TRAVEL</h1>
+        <p class="center">Eye Travel is Michigans first and only eye care practice that specializes in home care.
+            Our doctors are able to provide our patients with the same care that they would receive in a traditional eye doctors office.
+            All of the equipment they need fits nicely in a suitcase, and is so portable that the exam can given whether
+            the patient prefers to sit in their favorite chair or even if they are bed bound.
+            Whether you're a home care professional, a caregiver, or just a friend of someone
+            who has difficulty getting to the doctors, contact Eye Travel today and help improve the
+            quality of life of someone you care about.
+            Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
+
+        <div class="row">
+            <div class="column-2 shmush-right">
+                <h1>"Eye Travel is the first and only eye care practice in Michigan to focus 100% on home care"</h1>
+            </div>
+            <div class="column-2 shmush-left">
+                <p>Eye Travel is Michigan's first and only eye care practice that specializes in home care.
+                    Our doctors are able to provide our patients with the same care that they would receive
+                    in a traditional eye doctors office. All of the equipment they need fits nicely in a suitcase,
+                    and is so portable that the exam can be given whether the patient prefers to sit in their favorite
+                    chair or even if they are bed bound.</p>
+            </div>
+        </div>
+    </div>
+    <br>
+        <br>
+    </div>
+
+    <div class="service-area center">
+
+        <h1>OUR SERVICE AREA</h1>
+        <!-- here we put the map once we learn how to use the google maps api:) -->
+        <h5>Eye Travel sees patients throughout Southeastern Michigan, we go wherever we are needed. We spend most of time in Detroit, MI and the surrounding areas. Our doctors do leave the Metro Detroit area a few times a month and come to Flint and Ann Arbor.</h5>
+        <br>
+        <h5>If you want to schedule an appointment or check to see if we come to your area, give us a call at (248) 376-6046.</h5>
+
+
+    </div>
+
     <div class="big-blue-stripe">
         <br>
     </div>
 
-<div>
-    <h2>WHAT IS EYE TRAVEL</h2>
-    <p>Eye Travel is Michigans first and only eye care practice that specializes in home care. 
-        Our doctors are able to provide our patients with the same care that they would receive in a traditional eye doctors office. 
-        All of the equipment they need fits nicely in a suitcase, and is so portable that the exam can given whether 
-        the patient prefers to sit in their favorite chair or even if they are bed bound. 
-        Whether you're a home care professional, a caregiver, or just a friend of someone 
-        who has difficulty getting to the doctors, contact Eye Travel today and help improve the 
-        quality of life of someone you care about. 
-        Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
-    
-    <div class="row">
-        <div class="column-2 shmush-right">
-            <h1>"Eye Travel is the first and only eye care practice in Michigan to focus 100% on home care"</h1>
-        </div>
-        <div class="column-2 shmush-left">
-            <p>Eye Travel is Michigan's first and only eye care practice that specializes in home care. 
-                Our doctors are able to provide our patients with the same care that they would receive 
-                in a traditional eye doctors office. All of the equipment they need fits nicely in a suitcase, 
-                and is so portable that the exam can be given whether the patient prefers to sit in their favorite 
-                chair or even if they are bed bound.</p>
-        </div>
+    <div class="center">
+        <h1>MEET DR YAAKOV ZACKS, O.D.</h1>
+        <img class="about-pic" src="about.jpg">
+
+        <p>Yaakov Zacks OD saw his first home bound patient in 2006.
+            He has been exclusively serving this largely under served population since then.</p>
+
+        <p>Dr. Zacks attended the New England College of Optometry in Boston, MA.
+            He held internships in community health centers and Boston VA hospital.
+            He has given thousands of eye exams to home-bound patients since committing to homecare in 2006.</p>
     </div>
-    
-    
+
+
+    <div class="big-blue-stripe">
+        <div class="row">
+            <br>
+            <h1 class="center white-text">WHAT OUR PATIENTS SAY</h1>
+            <div class="column">
+                <div class="card">
+                    <p>"I HAVEN'T HAD MY EYES CHECKED IN 6 YEARS SINCE I HAD MY STROKE, NOW I CAN GET THEM CHECKED YEARLY"</p>
+                    <p class="feedback_name">Lois W.</p>
+                    <p>Detroit, MI</p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <p>"SO QUICK AND THOROUGH; IF I WENT TO THE DOCTOR'S OFFICE I WOULD BE THERE FOR HOURS"</p>
+                    <p class="feedback_name">Willie K.</p>
+                    <p>Lansing, MI</p>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <p>"THANKS A MILLION, I DON'T KNOW WHAT I WOULD'VE DONE WITHOUT AN EYE DOCTOR COMING TO MY HOUSE"</p>
+                    <p class="feedback_name">Joshua R.</p>
+                    <p>Taylor, MI</p>
+                </div>
+            </div>
+        </div>
+        <br><br>
     </div>
+
+    <div class="center">
+        <h1>SUBMIT A REFERRAL</h1>
+        <img class="referral" src="referral.pic">
+        <p>If you are a home care professional and would like us to see a patient of yours, 
+            you can fax the information to (248) 569-7054.</p>
+    </div>
+
 </body>
 
 </html>
