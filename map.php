145154fb946ae9e7f91ecd7f32c4cffd89911a1d
Shira Zacks <shiramzacks@gmail.com>
2022-10-25 17:35:10 -0400

    map hac correct cordinates yayyy!!!

--- map.php
+++ map.php
@@ -5,27 +5,27 @@
 
     <h1>My First Google Map</h1>
 
-    <div id="map" style="width:100%;height:400px;"></div>
+    <div id="map" style="width:50%;height:400px;"></div>
 
     <script>
         // This example creates a simple polygon representing the Bermuda Triangle.
         function initMap() {
             const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 5,
+                zoom: 8,
                 center: {
-                    lat: 24.886,
-                    lng: -70.268
+                    lat:42.52, //wixom 		
+                    lng: -83.53
                 },
                 mapTypeId: "terrain",
             });
             // Define the LatLng coordinates for the polygon's path.
             const triangleCoords = [{
-                    lat: 42.38,//detroit
-                    lng: -83.1
+                    lat:42.334185,//detroit
+                    lng: -83.041567
                 },
                 {
-                    lat: 42.99,	
-                    lng: -82.43//port huron
+                    lat: 41.75, //lambertville  			
+                    lng: -83.63 
                 },
                 {
                     lat: 42.01,	//tecumsah 	
@@ -40,17 +40,25 @@
                     lng: -84.56
                 },
                 {
-                    lat: 42.38,//detroit
-                    lng: -83.1
+                    lat: 43.02, //flint  		
+                    lng: -83.69
+                },
+                {
+                    lat: 42.99,	
+                    lng: -82.43//port huron
+                },
+                {
+                    lat:42.334185,//detroit
+                    lng: -83.0415
                 }
             ];
             // Construct the polygon.
             const bermudaTriangle = new google.maps.Polygon({
                 paths: triangleCoords,
-                strokeColor: "#FF0000",
+                strokeColor: "#0191d7",
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
-                fillColor: "#FF0000",
+                fillColor: "#0191d7",
                 fillOpacity: 0.35,
             });
 
@@ -60,7 +68,7 @@
         window.initMap = initMap;
     </script>
 
-    <    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVL3o1VE9oI0tih1Vlh2-gvS0ShlFpvDU&callback=initMap"></script></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVL3o1VE9oI0tih1Vlh2-gvS0ShlFpvDU&callback=initMap"></script>
 
 </body>
 
