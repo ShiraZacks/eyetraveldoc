f831dc87d2cef7315cf79ab1144931ec68e97311
Shira Zacks <shiramzacks@gmail.com>
2022-10-27 18:06:57 -0400

    link fixed

--- 404.php
+++ 404.php
@@ -13,9 +13,11 @@
 
 <body>
     <div class="div404 center">
+    <br><br><br>
     <h1>Something went wrong!</h1>
     <h2>This is a 404 error, which means that the link you clicked is broken. The page you are trying to access does not exist.</h2>
-    <h3 class="link404"><a href="index.php">Click here to return to the Eye Travel homepage.</a></h3>
+    <h3 ><a class="link404" href="index.php">Click here to return to the Eye Travel homepage.</a></h3>
+    <br><br><br>
 </div>
 </body>
 </html>
