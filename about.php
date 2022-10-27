a61598da90bebb1d2e77dd96a85338cee9eb4945
Shira Zacks <shiramzacks@gmail.com>
2022-10-27 18:11:07 -0400

    adding about text

--- about.php
+++ about.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>What is Eye Travel</title>
+    <title>Eye Travel: Eye Care For The Home Bound</title>
+    <link rel="shortcut icon" type="image/png" href="eye.png" />
+    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
+    <link rel="stylesheet" href="style.css?<?= time() ?>">
 </head>
+<?php include "menu.php"; ?>
+
 <body>
-    <h1>What is Eye Travel?</h1>
-    <p>good question. Hopefully we will answer it soon.</p>
+    <h1>What is Eye Travel</h1>
+        <p>Eye Travel is Michigan's first and only eye care practice that specializes in-home care. 
+            Our doctors are able to provide our patients with the same care that they would receive in a 
+            traditional eye doctor's office. All of the equipment they need fits nicely in a suitcase and 
+            is so portable that the exam can be given whether the patient prefers to sit in their favorite 
+            chair or even if they are bed-bound.</p>
+        <p>Whether you're a home care professional, a caregiver, or just a friend of someone who has difficulty 
+            getting to the doctors contact Eye Travel today and help improve the quality of life of someone 
+            you care about. Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
+        <p> As an added measure of convenience, our doctors also bring with them a selection of men's 
+            and women's glasses. Once the patient selects the frames that they like, the doctor will 
+            make the adjustments to the frame to make sure that it fits correctly, and the glasses will 
+            be delivered within 7-10 days.</p>
+
 </body>
 </html>
