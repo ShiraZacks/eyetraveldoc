887aac7ac1ce4e2c1da15c0cd0715cf8bcaae0a9
Shira Zacks <shiramzacks@gmail.com>
2022-10-28 16:17:35 -0400

    about edits

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,19 +13,21 @@
 <?php include "menu.php"; ?>
 
 <body>
-    <h1>What is Eye Travel</h1>
-        <p>Eye Travel is Michigan's first and only eye care practice that specializes in-home care. 
-            Our doctors are able to provide our patients with the same care that they would receive in a 
-            traditional eye doctor's office. All of the equipment they need fits nicely in a suitcase and 
-            is so portable that the exam can be given whether the patient prefers to sit in their favorite 
+    <div class="about-div center">
+        <h1>What is Eye Travel</h1>
+        <p>Eye Travel is Michigan's first and only eye care practice that specializes in-home care.
+            Our doctors are able to provide our patients with the same care that they would receive in a
+            traditional eye doctor's office. All of the equipment they need fits nicely in a suitcase and
+            is so portable that the exam can be given whether the patient prefers to sit in their favorite
             chair or even if they are bed-bound.</p>
-        <p>Whether you're a home care professional, a caregiver, or just a friend of someone who has difficulty 
-            getting to the doctors contact Eye Travel today and help improve the quality of life of someone 
+        <p>Whether you're a home care professional, a caregiver, or just a friend of someone who has difficulty
+            getting to the doctors contact Eye Travel today and help improve the quality of life of someone
             you care about. Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
-        <p> As an added measure of convenience, our doctors also bring with them a selection of men's 
-            and women's glasses. Once the patient selects the frames that they like, the doctor will 
-            make the adjustments to the frame to make sure that it fits correctly, and the glasses will 
+        <p> As an added measure of convenience, our doctors also bring with them a selection of men's
+            and women's glasses. Once the patient selects the frames that they like, the doctor will
+            make the adjustments to the frame to make sure that it fits correctly, and the glasses will
             be delivered within 7-10 days.</p>
-
+    </div>
 </body>
+
 </html>
