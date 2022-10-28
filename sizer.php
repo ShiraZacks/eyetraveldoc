81a8a1284b7f589f6ca1441e685f3757d10138d2
Shira Zacks <shiramzacks@gmail.com>
2022-10-28 16:32:23 -0400

    adding size changer

---
+++ sizer.php
@@ -0,0 +1,11 @@
+<script>
+function increaseFontSizeBy30px() {
+        document.getElementById('enlarge').style.fontSize = "30px";
+    }
+</script>
+
+<div>
+
+<input type="button" value="Increase Font Size 30px" onclick="increaseFontSizeBy30px()">
+<p id="enlarge">Font Size</p>
+</div>
