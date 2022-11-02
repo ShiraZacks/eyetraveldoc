5dc2afd8152293943b30cb27dcadb6543b7b2375
Shira Zacks <shiramzacks@gmail.com>
2022-11-02 10:52:17 -0400

    added pic of dr zacks with diploma playing with sizing

--- sizer.php
+++ sizer.php
@@ -1,11 +1,18 @@
 <script>
-function increaseFontSizeBy30px() {
-        document.getElementById('enlarge').style.fontSize = "30px";
+function bigger() {
+        document.getElementsByClassName('bigger').style.fontSize = "30px";
+        console.log("clicked bigger")
     }
+
+function smaller() {
+    document.getElementsByClassName('smaller').style.fontSize = "10px";
+    console.log("clicked smaller")
+    element.getElementsByClassName.style.fontSize = "24px";
+}
 </script>
 
 <div>
-
-<input type="button" value="Increase Font Size 30px" onclick="increaseFontSizeBy30px()">
-<p id="enlarge">Font Size</p>
+<input type="button" value="bigger" onclick="bigger()">
+<input type="button" value="smaller" onclick="smaller()">
+<p class="bigger smaller">Font Size</p>
 </div>
