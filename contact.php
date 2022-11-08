1d2129017df148460682f9bba7ba3f18b4e573a1
Shira Zacks <shiramzacks@gmail.com>
2022-11-08 17:08:41 -0500

    fixed contact-edit to be contact

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,19 +8,55 @@
     <title>Eye Travel: Eye Care For The Home Bound</title>
     <link rel="shortcut icon" type="image/png" href="eye.png" />
     <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script> <!-- this is cuz of the recaptcha, hopefully it will work soon -->
+
     <link rel="stylesheet" href="style.css?<?= time() ?>">
 </head>
 <?php include "menu.php"; ?>
 
 <body>
-    <div class="div404 center">
-    <br><br><br>
-    <h1>Something went wrong!</h1>
-    <h2>This is a 404 error, which means that the link you clicked is broken or the page you are trying to access does not exist.</h2>
-    <h3 ><a class="link404" href="index.php">Click here to return to the Eye Travel homepage.</a></h3>
+<div>
+    <div class="big-blue-stripe"> <br><br>
+    <h1 class="contact white-text center">We want to hear from you!</h1>
     <br><br>
-</div><br>
-<?php include "footer.php"?>
+    </div>
+    <form method="post" action="https://formspree.io/f/mlevoaar" class="form ">
+        <?php if (isset($errors)) { ?>
+            <p><?php echo $errors; ?></p>
+        <?php } ?>
+        <fieldset class=" fieldset">
+            <legend> Contact Us Today! </legend>
+            <br><br>
+            <div    class="contact-div">
+            <label for="full-name">Name</label>
+            <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
+            <br><br>
+            <label for="email-address">Email Address</label>
+            <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
+            <br><br>
+            <label for="phone">Phone Number</label>
+            <input type="phone" name="phone" id="phone" placeholder="123456789" required="">
+            <br><br>
+            <label for="message">Message</label>
+            <textarea rows="4" name="message" id="message" placeholder="Type message here" required=""></textarea>
+            <br><br>
+            </div>
+            <input type="hidden" name="_subject" id="email-subject" value="Eye Travel Contact Form Submission">
+            <br>
+            <div class="center">
+            <div class="g-recaptcha" data-sitekey="6LcNQe0iAAAAAKMiSeu7-AW6XsNCl-gj5EXrujOr"></div> <!-- recaptcha is a im not a robot button, it  works!!!!!!! -->
+            </div>
+            <br>
+            <input type="submit" value="Submit" class="submit-button">
+            <br><br>
+            
+        </fieldset>
+
+    </form>
+</div>
+
+    <?php include "footer.php" ?>
 
 </body>
+
 </html>
