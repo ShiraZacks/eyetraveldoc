6d99fcb1cd5da755c56ef7d379245ff33f2e6656
Shira Zacks <shiramzacks@gmail.com>
2022-11-08 20:26:46 -0500

    worjing on making responsive design

--- index.php
+++ index.php
@@ -28,13 +28,13 @@
         <br><br>
         <h1 class="center">OUR SERVICE</h1>
         <hr class="white">
-        <h3 class="center">Eye Travel has focused on providing residents of Southeastern Michigan with 
-            top-quality eye care. Using state of the art portable medical equipment, each patient receives 
-            a comprehensive eye exam. The exam begins with the doctor testing the patient's vision. The doctor 
+        <h3 class="center">Eye Travel has focused on providing residents of Southeastern Michigan with
+            top-quality eye care. Using state of the art portable medical equipment, each patient receives
+            a comprehensive eye exam. The exam begins with the doctor testing the patient's vision. The doctor
             continues to give a full eye exam, checking the overall health of the eye.</h3>
-        <h3 class="center">If you have any questions you can call our office at (248)376-6046 or send an 
+        <h3 class="center">If you have any questions you can call our office at (248)376-6046 or send an
             email to drzacks@eyetraveldoc.com</h3>
-            <br><br>
+        <br><br>
     </div>
     <br><br>
 
@@ -67,48 +67,48 @@
             </div>
             <ul><b>SCHEDULE APPOINTMENT</b></ul>
             <hr>
-            <p class="appt-text">If you, or someone you care for, needs to schedule an appointment with Eye Travel, 
+            <p class="appt-text">If you, or someone you care for, needs to schedule an appointment with Eye Travel,
                 call (248) 376-6046 and we will be in touch with you as soon as possible</p>
         </div>
     </div>
     <br>
     <div class="big-blue-stripe">
         <br>
-    <div class="white-text">
-        <h1 class="center">WHAT IS EYE TRAVEL</h1>
-        <p class="center">Eye Travel is Michigans first and only eye care practice that specializes in home care.
-            Our doctors are able to provide our patients with the same care that 
-            they would receive in a traditional eye doctors office.
-            All of the equipment they need fits nicely in a suitcase, and is so portable that the exam can given whether
-            the patient prefers to sit in their favorite chair or even if they are bed bound.
-            Whether you're a home care professional, a caregiver, or just a friend of someone
-            who has difficulty getting to the doctors, contact Eye Travel today and help improve the
-            quality of life of someone you care about.
-            Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
+        <div class="white-text">
+            <h1 class="center">WHAT IS EYE TRAVEL</h1>
+            <p class="center">Eye Travel is Michigans first and only eye care practice that specializes in home care.
+                Our doctors are able to provide our patients with the same care that
+                they would receive in a traditional eye doctors office.
+                All of the equipment they need fits nicely in a suitcase, and is so portable that the exam can given whether
+                the patient prefers to sit in their favorite chair or even if they are bed bound.
+                Whether you're a home care professional, a caregiver, or just a friend of someone
+                who has difficulty getting to the doctors, contact Eye Travel today and help improve the
+                quality of life of someone you care about.
+                Dr. Zacks and his staff see patients all over Southeastern Michigan.</p>
 
-        <div class="row">
-            <div class="column-2 shmush-right">
-                <h1>"Eye Travel is the first and only eye care practice in Michigan to focus 100% on home care"</h1>
-            </div>
-            <div class="column-2 shmush-left">
-                <p>Eye Travel is Michigan's first and only eye care practice that specializes in home care.
-                    Our doctors are able to provide our patients with the same care that they would receive
-                    in a traditional eye doctors office. All of the equipment they need fits nicely in a suitcase,
-                    and is so portable that the exam can be given whether the patient prefers to sit in their favorite
-                    chair or even if they are bed bound.</p>
+            <div class="row">
+                <div class="column-2 shmush-right">
+                    <h1>"Eye Travel is the first and only eye care practice in Michigan to focus 100% on home care"</h1>
+                </div>
+                <div class="column-2 shmush-left">
+                    <p>Eye Travel is Michigan's first and only eye care practice that specializes in home care.
+                        Our doctors are able to provide our patients with the same care that they would receive
+                        in a traditional eye doctors office. All of the equipment they need fits nicely in a suitcase,
+                        and is so portable that the exam can be given whether the patient prefers to sit in their favorite
+                        chair or even if they are bed bound.</p>
+                </div>
             </div>
         </div>
-    </div>
-    <br>
+        <br>
         <br>
     </div>
 
     <div class="service-area center">
 
         <h1>OUR SERVICE AREA</h1>
-        <?php include "map.php"?>
-        <h3>Eye Travel sees patients throughout Southeastern Michigan, we go wherever we are needed. 
-            We spend most of time in Detroit, MI and the surrounding areas. 
+        <?php include "map.php" ?>
+        <h3>Eye Travel sees patients throughout Southeastern Michigan, we go wherever we are needed.
+            We spend most of time in Detroit, MI and the surrounding areas.
             Our doctors do leave the Metro Detroit area a few times a month and come to Flint and Ann Arbor.</h3>
         <br>
         <p>If you want to schedule an appointment or check to see if we come to your area, give us a call at (248) 376-6046.</p>
@@ -117,7 +117,8 @@
     </div>
 
     <div class="big-blue-stripe">
-        <br><br><hr class="white"><br><br>
+        <br><br>
+        <hr class="white"><br><br>
     </div>
 
     <div class="center">
@@ -167,11 +168,11 @@
     <div class="center">
         <h1>SUBMIT A REFERRAL</h1>
         <img class="referral" src="referral.jpg">
-        <p>If you are a home care professional and would like us to see a patient of yours, 
+        <p>If you are a home care professional and would like us to see a patient of yours,
             you can fax the information to (248) 569-7054.</p>
     </div>
 
-<?php include "footer.php"?>
+    <?php include "footer.php" ?>
 
 </body>
 
