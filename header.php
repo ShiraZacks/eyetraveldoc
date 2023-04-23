75c0bd0288ce99467dc99a62af1169375935a969
Shira Zacks <96266721+ShiraZacks@users.noreply.github.com>
2023-04-23 17:06:39 -0400

    took away page extentions

--- header.php
+++ header.php
@@ -9,9 +9,9 @@ echo ' <!-- Navbar  -->
 
 
 
-            <a href="index.php" class="buttons">Home</a>
-            <a href="about.php" class="buttons">What is Eye Travel</a>
-            <a href="contact.php" class="buttons">Contact</a>
+            <a href="index" class="buttons">Home</a>
+            <a href="about" class="buttons">What is Eye Travel</a>
+            <a href="contact" class="buttons">Contact</a>
             <a href="https://portal.aprima.com/11/login.aspx?UID=a747b803-8d58-479c-8365-87a8f087e10e" class="buttons">Patient Portal</a>
         </div>
     <!-- eye travel logo-->
@@ -22,9 +22,9 @@ echo ' <!-- Navbar  -->
         <!-- Navbar on small screens -->
         <div id="smallScreen" class="hide">
             <ul>
-                <li><a href="index.php" class="buttonSmall">Home</a></li>
-                <li><a href="about.php" class="buttonSmall">What is Eye Travel</a></li>
-                <li><a href="contact.php" class="buttonSmall">Contact</a></li>
+                <li><a href="index" class="buttonSmall">Home</a></li>
+                <li><a href="about" class="buttonSmall">What is Eye Travel</a></li>
+                <li><a href="contact" class="buttonSmall">Contact</a></li>
                 <li><a href="https://portal.aprima.com/11/login.aspx?UID=a747b803-8d58-479c-8365-87a8f087e10e" class="buttonSmall">Patient Portal</a></li>
             </ul>
         </div>
