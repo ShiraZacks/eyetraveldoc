23f47291b092eb30a31df9c0d1e526bbe8b39907
Shira <shiramzacks@gmail.com>
2023-04-27 12:01:01 -0400

    added meta descriptions and titles

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Eye Travel is Michigan's first and only eye care practice to focus 100% on home care. Contact us to schedule an appointment!">
+    <meta title="Contact Eye Travel: Eye Care For The Home Bound">
     <title>Eye Travel: Eye Care For The Home Bound</title>
     <link rel="shortcut icon" type="image/png" href="eye.png" />
     <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta title="Eye Travel: Eye Care For The Home Bound">
-    <meta name="description" content="Eye Travel is the first and only eye care practice to focus 100% on home care.">
+    <meta name="description" content="Eye Travel is Michigan's first and only eye care practice to focus 100% on home care. A full eye exam in the comfort of your favorite chair!">
     <title>Eye Travel: Eye Care For The Home Bound</title>
     <link rel="shortcut icon" type="image/png" href="eye.png" />
     <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
